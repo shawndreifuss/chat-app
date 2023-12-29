e4e7824d2f2b55ad22364acf249a45fe4d5a1a7a
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-12-28 16:50:36 -0800

    added chat navbar with mui icons

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Shawn</span>
+        <div className="chatIcons">
+          <VideocamIcon className="img" />
+          <PersonAddIcon className="img" />
+          <MoreHorizIcon className="img" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
